8c0bd4b9c83bb6ef3b9064a157cdee80b5ad77f3
taylor901 <46793431+taylor901@users.noreply.github.com>
2019-04-02 18:02:45 -0500

    Delete views.py

--- blog/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Post
-
-def post_list(request):
-    # This is a QuerySet that returns all the posts ordered by the publish date
-    posts = Post.objects.order_by('published_date')
-    # Pass the posts to the template
-    return render(request, 'blog/post_list.html', {'posts': posts})
-# Create your views here.
